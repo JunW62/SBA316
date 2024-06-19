8b8696b9d9d2de1d43bf353e35946be5fc969518
JunW62 <sivia1990@gmail.com>
2024-06-19 14:03:41 -0500

    add funcs

--- index.js
+++ index.js
@@ -14,3 +14,45 @@ function playGame(event) {
   const winner = getWinner(playerChoice, computerChoice);
   showResult(playerChoice, computerChoice, winner);
 }
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+function getWinner(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return "draw";
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return "player";
+  } else {
+    return "computer";
+  }
+}
+function showResult(playerChoice, computerChoice, winner) {
+  if (winner === "player") {
+    playerScore++;
+    resultEl.textContent = `You Win! ${capitalize(
+      playerChoice
+    )} beats ${capitalize(computerChoice)}.`;
+  } else if (winner === "computer") {
+    computerChoice++;
+    resultEl.textContent = `You Lose! ${capitalize(
+      computerChoice
+    )} beats ${capitalize(playerChoice)}.`;
+  } else {
+    resultEl.textContent = `It is a Draw! You both chose ${capitalize(
+      playerChoice
+    )}.`;
+  }
+  playerScoreEl.textContent = playerScore;
+  computerScoreEl.textContent = computerScore;
+}
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
