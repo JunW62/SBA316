84fd1c1200cdaa54b279ff46086a5a25d8ac03e1
JunW62 <sivia1990@gmail.com>
2024-06-19 13:31:55 -0500

    get element and add eventListener

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const playScoreEl = document.getElementById("player-score");
+const playerScoreEl = document.getElementById("player-score");
+const computerScoreEl = documnet.getElementById("computuer-score");
+const resultEl = document.getElementById("result");
+const choices = document.getElementById("#choised button");
+
+let computerScore = 0;
+let playerScore = 0;
+
+choices.forEach((button) => addEventListener("click", playGame));
+
+function playGame(event) {
+  const playerChoice = event.target.id;
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice, computerChoice);
+  showResult(playerChoice, computerChoice, winner);
+}
